69b79d51973d1c96c6898ffdf056c8512a79d362
Damian Tarczoń <tarczon96@gmail.com>
2022-08-16 14:02:02 +0200

    adding css to navbar - not complete!

--- friendlygames-frontend/src/components/Navbar.js
+++ friendlygames-frontend/src/components/Navbar.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Navbar(){
     return (
         <nav>
-            <h2>friendlygames</h2>
-            <div className="searching-bar">
+            <h2 className="app--name">friendlygames</h2>
+            <div className="searching--bar">
                 <button type="button" className="button-where" >Gdziekolwiek</button>
                 <button type="button" className="button-when" >Kiedy</button>
                 <button type="submit" className="button-submit" >Submit</button>
             </div>
-            <p>Logowanie</p>
-            <p>Rejestracja</p>
+            <p className="login">Logowanie</p>
+            <p className="register">Rejestracja</p>
         </nav>
     )
 }
