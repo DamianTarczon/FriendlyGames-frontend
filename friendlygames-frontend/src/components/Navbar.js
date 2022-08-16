5a6bcd0b1e70ec3e912cb92f646b1efb63c99d35
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-08-16 15:04:41 +0200

    searching bar css not complete

--- friendlygames-frontend/src/components/Navbar.js
+++ friendlygames-frontend/src/components/Navbar.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Navbar(){
     return (
-        <nav>
+        <nav className="navbar">
             <h2 className="app--name">friendlygames</h2>
             <div className="searching--bar">
-                <button type="button" className="button-where" >Gdziekolwiek</button>
-                <button type="button" className="button-when" >Kiedy</button>
-                <button type="submit" className="button-submit" >Submit</button>
+                <button type="button" id="button--search" >Gdziekolwiek</button>
+                <button type="button" id="button--search" >Dowolny tydzień</button>
+                <button type="submit"><i className="fa fa-search"></i></button>
             </div>
             <p className="login">Logowanie</p>
             <p className="register">Rejestracja</p>
