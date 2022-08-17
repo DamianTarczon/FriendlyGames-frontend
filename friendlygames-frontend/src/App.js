b9b1ef954a62b4c26f03b0f7b95b45f95203e941
adrianzych2clo <adrianzych2@gmail.com>
2022-08-17 14:29:30 +0200

    changed size of footer

--- friendlygames-frontend/src/App.js
+++ friendlygames-frontend/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.js";
 import React from 'react';
 import Searchbar from "./components/Searchbar.js";
 import Footer from "./components/Footer.js";
+import MainView from "./components/Mainview.js";
 
 export default function App() {
   return (
