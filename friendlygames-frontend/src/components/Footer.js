a6743ad4120e7eebcc831675046d62b518a2371e
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-08-17 11:43:32 +0200

    add footer done

---
+++ friendlygames-frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div className="footer">
+            <button className="footer--button">O nas</button>
+            <button className="footer--button">FAQ</button>
+            <button className="footer--button">Polityka prywatności</button>
+            <button className="footer--button">Kontakt</button>
+            <button className="footer--button"><b>friendlygames</b></button>
+            <hr className="footer--hr"></hr>
+        </div>
+    )
+}
