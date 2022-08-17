8f5f6af62054b1e6c0052bce3b1bee3e9f530efb
adrianzych2clo <adrianzych2@gmail.com>
2022-08-17 14:19:11 +0200

    added main view

--- friendlygames-frontend/src/App.js
+++ friendlygames-frontend/src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.js"
 import React from 'react';
 import Searchbar from "./components/Searchbar.js";
+import MainView from "./components/Mainview.js";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Searchbar />
+      <MainView />
     </div>
   );
 }
