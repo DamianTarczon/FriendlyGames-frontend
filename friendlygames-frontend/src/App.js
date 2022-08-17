6b449340087f0f254567d13ba00870e48edfa43a
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-08-17 14:41:29 +0200

    add registration page

--- friendlygames-frontend/src/App.js
+++ friendlygames-frontend/src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar.js";
 import React from 'react';
 import Searchbar from "./components/Searchbar.js";
 import Footer from "./components/Footer.js";
+import RegistrationPage from "./components/RegistrationPage.js";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Searchbar />
+      
+      <RegistrationPage />
       <Footer />
     </div>
   );
--- friendlygames-frontend/src/components/SearchingCard.js
+++ friendlygames-frontend/src/components/SearchingCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Searchbar(props){
-    console.log(props)
     return (
         <div>
             <img src={`/images/${props.img}`} alt="img" className="card--image" />
