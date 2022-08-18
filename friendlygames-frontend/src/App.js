69e793566a890f81d0b5a4fbcadb447c95dbcbb5
Damian Tarczoń <tarczon96@gmail.com>
2022-08-18 10:48:53 +0200

    searching bar for events added udner the navbar

--- friendlygames-frontend/src/App.js
+++ friendlygames-frontend/src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./components/Navbar.js";
 import React from 'react';
-// import Searchbar from "./components/Searchbar.js";
+import Searchbar from "./components/Searchbar.js";
 // import Footer from "./components/Footer.js";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <div><p>cokolwiek</p></div>
+      <Searchbar />
     </div>
   );
 }
