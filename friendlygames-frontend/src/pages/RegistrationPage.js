51d05e7146a863b469db3840c7200f0369cbaa85
Damian Tarczoń <tarczon96@gmail.com>
2022-09-14 15:03:20 +0200

    registration page improved (connected with backend)

--- friendlygames-frontend/src/pages/RegistrationPage.js
+++ friendlygames-frontend/src/pages/RegistrationPage.js
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RegistrationPage(){
+    const [registerData, setRegisterData] = useState({
+        email: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        roles: ["User"]
+    })
+
+    function handleChange(event) {
+        setRegisterData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const userData = {
+            method: 'OPTIONS',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(registerData)
+        };
+        fetch('https://localhost:7089/api/Users/register', userData)
+    }
+
     return (
         <div className="registrationPage">
-            <img src={`/images/register-photo.png`} alt="img" className="registration--image" />
+            <div className="registration--imageDiv">
+                <img src={`/images/register-photo.png`} alt="img" className="registration--image" />
+            </div>
             <div className="registration--data">
-                <p>Twój nick:</p>
-                <input type="text" className="input"></input>
-                <p>Adres e-mail:</p>
-                <input type="text" className="input" placeholder="jankowalski@gmail.com"></input>
-                <p>Hasło:</p>
-                <input type="password"className="input"></input>
-                <br></br>
-                <br></br>
-                <button type="submit" className="registration--button">Załóż konto</button>
+                <form onSubmit={handleSubmit}>
+                    <p>Email:</p>
+                    <input 
+                    type="text" 
+                    placeholder="user@example.com"
+                    onChange={handleChange}
+                    name="email"
+                    value={registerData.email}
+                    className="registration--email"
+                    />
+                    <p>Imię:</p>
+                    <input 
+                    type="text" 
+                    placeholder="Jan"
+                    onChange={handleChange}
+                    name="firstName"
+                    value={registerData.firstName}
+                    className="registration--firstName"
+                    />
+                    <p>Nazwisko:</p>
+                    <input 
+                    type="text" 
+                    placeholder="Kowalski"
+                    onChange={handleChange}
+                    name="lastName"
+                    value={registerData.lastName}
+                    className="registration--lastName"
+                    />
+                    <p>Hasło:</p>
+                    <input 
+                    type="password" 
+                    placeholder="*******"
+                    onChange={handleChange}
+                    name="password"
+                    value={registerData.password}
+                    className="registration--password"
+                    />
+                    <button className="registration--button">Zarejestruj się</button>
+                </form>
             </div>
         </div>
     )
